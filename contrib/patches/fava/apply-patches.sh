9063213db5ede1cff86d46d8a5ac65daa291d766
James Swineson <github@public.swineson.me>
2021-07-08 01:59:14 +0000

    fix double patching

--- contrib/patches/fava/apply-patches.sh
+++ contrib/patches/fava/apply-patches.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -Eeuo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 FAVA_DIR="/usr/local/lib/python3.7/dist-packages/fava"
 
 pushd "$FAVA_DIR"
 for PATCH_FILE in "${SCRIPT_DIR}/"*.patch; do
-    patch --backup-if-mismatch --strip=0 --forward < "$PATCH_FILE"
+    patch --backup-if-mismatch --strip=0 --forward < "$PATCH_FILE" || true
 done
